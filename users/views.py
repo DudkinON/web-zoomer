02152313ea22c0e261915e397c93d8d2ac63e3ab
Oleg Dudkin <dudkin.on@gmail.com>
2017-12-19 23:20:37 -0600

    feat: Add signin with provider

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from __future__ import unicode_literals
 
+from json import loads
 from hashlib import sha256 as hashed
+from httplib2 import Http
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+from requests import get as r_get
+
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.hashers import make_password, check_password
@@ -11,9 +16,11 @@ from django.utils import translation
 from django.utils.translation import ugettext_lazy as _, LANGUAGE_SESSION_KEY
 from django.utils.translation import get_language
 from django.views.generic import View
+from django.http import JsonResponse
 
 from blog.models import Article
-from web_zoomer_com.settings import SITE_URL
+from main.functions import get_unique_str
+from web_zoomer_com.settings import SITE_URL, BASE_DIR
 
 from .forms import UserLoginForm, UserRegisterForm
 from .models import User
@@ -116,6 +123,134 @@ class RegisterUserView(View):
             return render(request, self.template_name, args)
         else:
             return render(request, self.template_name, args)
+
+
+class OAuthUserView(View):
+    def post(self, request, provider):
+        # STEP 1 - Parse the auth code
+        code = request.data
+
+        if provider == 'google':
+            # STEP 2 - Exchange for a token
+            try:
+                # Upgrade the authorization code into a credentials object
+                g_file = ''.join([BASE_DIR, '/client_secrets.json'])
+                oauth_flow = flow_from_clientsecrets(g_file, scope='')
+                oauth_flow.redirect_uri = 'postmessage'
+                credentials = oauth_flow.step2_exchange(code)
+            except FlowExchangeError:
+                error = {'error': 'Failed to upgrade the authorization code.'}
+                return JsonResponse(error)
+
+            # Check that the access token is valid.
+            access_token = credentials.access_token
+            url = (
+                'https://www.googleapis.com/oauth2/v1/tokeninfo'
+                '?access_token=%s' %
+                access_token)
+            h = Http()
+            result = loads(h.request(url, 'GET')[1])
+            # If there was an error in the access token info, abort.
+            if result.get('error') is not None:
+                return JsonResponse({'error': 'server error'})
+
+            # Get user info
+            h = Http()
+            userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+            params = {'access_token': credentials.access_token, 'alt': 'json'}
+            answer = r_get(userinfo_url, params=params)
+
+            data = answer.json()
+
+            # see if user exists, if it doesn't make a new one
+            user = User.objects.get(email=data['email']) or None
+            if not user:
+                user = User.objects.create_user(
+                    username=data.get('name'),
+                    avatar=data.get('picture'),
+                    email=data.get('email'),
+                    first_name=data.get('given_name'),
+                    last_name=data.get('family_name'),
+                    password=get_unique_str(8)
+                )
+
+            # Make token
+            token = user.generate_auth_token()
+
+            # Send back token to the client
+            return JsonResponse({'token': token.decode('ascii'),
+                                 'uid': user.id,
+                                 'first_name': user.first_name,
+                                 'last_name': user.last_name,
+                                 'email': user.email,
+                                 'picture': user.picture,
+                                 'status': user.status,
+                                 'full_name': user.get_full_name}), 200
+
+        elif provider == 'facebook':
+
+            data = request.json.get('data')
+            access_token = data['access_token']
+            fb_file = ''.join([BASE_DIR, '/facebook.json'])
+            fb_data = loads(open(fb_file, 'r').read())['facebook']
+            app_id = fb_data['app_id']
+            app_secret = fb_data['app_secret']
+            url = fb_data['access_token_url'] % (
+                app_id, app_secret, access_token)
+            h = Http()
+            result = h.request(url, 'GET')[1]
+
+            # Use token to get user info from API
+
+            token = result.split(',')[0].split(':')[1].replace('"', '')
+            url = fb_data['user_info_url'] % token
+
+            h = Http()
+            result = h.request(url, 'GET')[1]
+            data = loads(result)
+            name = data['name'].split(' ')
+
+            user_data = dict()
+            user_data['provider'] = 'facebook'
+            user_data['username'] = data.get('name')
+            user_data['first_name'] = name[0]
+            user_data['last_name'] = name[1]
+            user_data['email'] = data.get('email')
+            user_data['facebook_id'] = data.get('id')
+            user_data['access_token'] = token
+
+            url = fb_data['picture_url'] % token
+            h = Http()
+            result = h.request(url, 'GET')[1]
+            data = loads(result)
+            user_data['picture'] = data['data']['url']
+            # login_session['picture'] = data["data"]["url"]
+
+            # see if user exists
+            user_info = User.objects.get(user_data['email']) or None
+
+            if user_info is None:
+                user_info = User.objects.create(
+                    username=user_data['username'],
+                    password=get_unique_str(8),
+                    first_name=user_data['first_name'],
+                    last_name=user_data['last_name'],
+                    email=user_data['email'],
+                    avatar=user_data['picture'])
+
+            g.user = user_info
+            token = g.user.generate_auth_token()
+            return JsonResponse({'token': token.decode('ascii'),
+                                 'uid': g.user.id,
+                                 'first_name': g.user.first_name,
+                                 'last_name': g.user.last_name,
+                                 'email': g.user.email,
+                                 'picture': g.user.picture,
+                                 'status': g.user.status,
+                                 'full_name': g.user.get_full_name}), 200
+
+        else:
+            return JsonResponse({'error': 'Unknown provider'}), 200
 
 
 def user_activation(request, uid, code):
