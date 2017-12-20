4ef82a4799829fab9cef2fdc55df18cd9d2eb43c
Oleg Dudkin <dudkin.on@gmail.com>
2017-12-19 21:32:31 -0600

    feat: Add oauth route

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,14 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 from .views import UsersLoginFormView, logout_view, RegisterUserView
-from .views import user_activation, UserProfile
+from .views import user_activation, UserProfile, OAuthUserView
 
 app_name = 'users'
 
 urlpatterns = [
     url(r'^login/', UsersLoginFormView.as_view(), name='login'),
     url(r'^register/', RegisterUserView.as_view(), name='register'),
+    url(r'^oauth/(?P<provider>\w+)', OAuthUserView.as_view(), name='oauth'),
     url(r'^logout/', logout_view, name='logout'),
     url(r'^profile/(?P<uid>\w+)/', UserProfile.as_view(), name='profile'),
     url(r'^activate/(?P<uid>\w+)/(?P<code>\w+)/', user_activation,
